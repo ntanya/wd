8f8806de44c67ef65324b89fa151020d7bb97e3a
Tanya N <ntanya@gmail.com>
2012-07-10 10:53:22 -0400

    updated string

--- twitter.js
+++ twitter.js
@@ -5,7 +5,7 @@ var mongo   = require('mongodb');
 var database = null;
 
 //var mongostr = "mongodb://localhost/dataintel";
-var mongostr = "mongodb://tanya:tanya@ds033897.mongolab.com:33757/heroku_app5667663"
+var mongostr = "mongodb://tanya:tanya@ds033757.mongolab.com:33757/heroku_app5667663"
 
 /*---- string trim helper ----*/
 String.prototype.trim = function() {
